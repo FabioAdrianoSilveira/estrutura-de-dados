ebbefb4f05d93147b7e9405159042ee7764b2fca
Fábio Adriano Silveira <fabioadrianosilveira456@gmail.com>
2025-08-19 20:29:36 -0300

    Correções do header + preparação dos arquivos cpp

---
+++ lista-de-exercicios-1/cadastro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "cadastro.h"
+
+
+bool cadastrarPessoa(Pessoa vetor[], int tamanho, string *nome, int idade, Sexo sexo, int *proximaPosicao) {
+    
+}
+
+void listarPessoas(Pessoa vetor[], int tamanho) {
+
+}
--- lista-de-exercicios-1/cadastro.h
+++ lista-de-exercicios-1/cadastro.h
@@ -1,22 +1,26 @@
-#define CADASTRO_H
 #ifndef CADASTRO_H
+#define CADASTRO_H
+
+#include <string>
+
+using namespace std;
 
 union Sexo
 {
-    int masculino,
-    int feminino,
-    int outro
+    int masculino;
+    int feminino;
+    int outro;
 };
 
 struct Pessoa
 {
-    string nome,
-    int idade,
-    Sexo sexo
+    string nome;
+    int idade;
+    Sexo sexo;
 };
 
-bool cadastrarPessoa(Pessoa vetor[], int tamanho, string *nome, int idade, Sexo sexo, int *proximaPosicao)
+bool cadastrarPessoa(Pessoa vetor[], int tamanho, string *nome, int idade, Sexo sexo, int *proximaPosicao);
 
-void listarPessoas(Pessoa vetor[], int tamanho)
+void listarPessoas(Pessoa vetor[], int tamanho);
 
 #endif
