7030c5de4fb0e207d9adcd67e3a98ca29aeb4bbc
Fábio Adriano Silveira <fabioadrianosilveira456@gmail.com>
2025-08-26 16:17:14 -0300

    Adicionando main

--- lista-de-exercicios-1/main.cpp
+++ lista-de-exercicios-1/main.cpp
@@ -4,6 +4,81 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    Pessoa pessoas[10];
+    int proximaPosicao = 0;
+    int opcao, idade, sexoInt;
+    string nome;
+    Sexo sexo;
+    bool cadastro;
+
+    do
+    {
+        cout << "\t ===MENU===" << endl;
+        cout << "1) Cadastrar pessoa" << endl;
+        cout << "2) Listar pessoa" << endl;
+        cout << "3) Sair" << endl;
+        cout << endl
+             << "Escolha uma opcao: ";
+        cin >> opcao;
+
+        switch (opcao)
+        {
+        case 1:
+            cout << "Digite o nome: ";
+            getline(cin.ignore(), nome);
+            cout << "Digite a idade: ";
+            cin >> idade;
+            cout << "Informe o sexo (0 = Masculino, 1 = Feminino, 2 = Outro)";
+            cin >> sexoInt;
+
+            switch (sexoInt)
+            {
+            case 0:
+                sexo.masculino = 1;
+                break;
+
+            case 1:
+                sexo.feminino = 1;
+                break;
+
+            case 2:
+                sexo.outro = 1;
+                break;
+
+            default:
+                cout << "Opcao invalida! Abortando cadastro";
+                main();
+                break;
+            }
+
+            cadastro = cadastrarPessoa(pessoas, 10, &nome, idade, sexo, &proximaPosicao);
+            if (cadastro == true)
+            {
+                cout << "Cadastro realizado com sucesso!\n";
+            }
+            else
+            {
+                cout << "Armazenamento cheio! Nao e possivel cadastrar mais pessoas\n";
+            }
+
+            break;
+
+        case 2:
+            listarPessoas(pessoas, 10);
+            break;
+
+        case 3:
+            cout << "Finalizando sistema";
+            break;
+
+        default:
+            cout << "Opcao invalida! Tente novamente\n";
+            main();
+            break;
+        }
+    } while (opcao != 3);
+
     return 0;
 }
