a313d9c6a4eedc7b5d440f08062f6eadf6f6e43e
Fábio Adriano Silveira <fabioadrianosilveira456@gmail.com>
2025-09-01 15:09:18 -0300

    Adiciona o arquivo cadastro.cpp

--- lista-de-exercicios-1/cadastro.cpp
+++ lista-de-exercicios-1/cadastro.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 #include "cadastro.h"
 
+bool cadastrarPessoa(Pessoa vetor[], int tamanho, string *nome, int idade, Sexo sexo, int *proximaPosicao)
+{
+    if (*proximaPosicao < tamanho)
+    {
+        vetor[*proximaPosicao].nome = *nome;
+        vetor[*proximaPosicao].idade = idade;
+        vetor[*proximaPosicao].sexo = sexo;
 
-bool cadastrarPessoa(Pessoa vetor[], int tamanho, string *nome, int idade, Sexo sexo, int *proximaPosicao) {
-    
+        (*proximaPosicao)++;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-void listarPessoas(Pessoa vetor[], int tamanho) {
+void listarPessoas(Pessoa vetor[], int tamanho)
+{
 
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (vetor[i].nome == "")
+        {
+            break;
+        }
+        else
+        {
+            cout << "\t PESSOA NUMERO " << i + 1 << endl;
+            cout << "Nome: " << vetor[i].nome << endl;
+            cout << "Idade: " << vetor[i].idade << endl;
+            if (vetor[i].sexo == masculino)
+            {
+                cout << "Sexo: Masculino" << endl
+                     << endl;
+            }
+            else if (vetor[i].sexo == feminino)
+            {
+                cout << "Sexo: Feminino" << endl
+                     << endl;
+            }
+            else if (vetor[i].sexo == outro)
+            {
+                cout << "Sexo: Outro" << endl
+                     << endl;
+            }
+        }
+    }
 }
